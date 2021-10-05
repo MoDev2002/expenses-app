dfd531316007b64a502d23d6c28ce950294f431a
Mohamed El Masry <momamedalmasry@gmail.com>
2021-10-05 14:43:54 +0200

    fixed an error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../expenseapp/lib/main.dart';
+import '../../expenses-app/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
